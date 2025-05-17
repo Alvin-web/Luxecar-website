eafb92a1caca04f23e8e50e35d8c7c98ce306ebc
Uduje Alvin <108984485+Alvin-web@users.noreply.github.com>
2025-05-17 11:00:56 +0100

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,25 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+
+const carousel = document.getElementById("carousel");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+
+nextBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: 300, behavior: "smooth" });
+});
+
+prevBtn.addEventListener("click", () => {
+  carousel.scrollBy({ left: -300, behavior: "smooth" });
+});
